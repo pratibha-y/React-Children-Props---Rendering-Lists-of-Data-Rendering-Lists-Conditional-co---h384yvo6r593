d7b2bb98c597094cf010a1dd0dd8ceb0b28481eb
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-03-22 16:12:49 +0530

    Update ChildComponent.js

--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const ChildComponent = ({setSquare}) => {
-  //code here
-  const findSquare = (event)=>{
-  let value = event.target.value;
-  setSquare(value*value);
-}
+function ChildComponent(props) {
+  const { inputValue, handleInputChange } = props;
+
+  const handleSquare = () => {
+    const squareValue = inputValue ** 2;
+    return squareValue;
+  };
+
   return (
     <div id="child">
-      Find The Square <br/>
-      <input id='input' onChange={findSquare} />
-     
+      Find The Square <br />
+      <input
+        id="input"
+        type="number"
+        onChange={handleInputChange}
+        value={inputValue}
+      />
+      <p id="output">{handleSquare()}</p>
     </div>
-  )
+  );
 }
-
-
 export default ChildComponent;
