0be9fa24c1de912bc15d9f7841a7d6533b7f3d8f
pratibha-y <76511037+pratibha-y@users.noreply.github.com>
2023-03-22 15:40:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,15 @@ import React , {useState,useEffect} from 'react'
 import '../styles/App.css';
 import ChildComponent from './ChildComponent';
 const App = () => {
- //code here
-
+  const [output, setOutput] = useState(0);
   return (
     <div id="main">
-      <ChildComponent /> 
-       <input id='input' onChange={} />
-    
-      <p id='output'>{}</p>
+      <ChildComponent setOutput={setOutput}/>
+      <p id='output'>{output}</p>
     </div>
   )
 }
 
 
 export default App;
+			
